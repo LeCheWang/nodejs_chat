4f613f46df067642d101020141918184de05ee98
LeCheWang <haphonggoroki@gmail.com>
2023-07-04 10:37:14 +0700

    video 2

--- public/js/client.js
+++ public/js/client.js
@@ -9,23 +9,50 @@ const ul_message = document.getElementById("ul_message")
 
 var socket = io.connect()
 
+let my_name = "";
+
 socket.on("connect", function(data){
     console.log(data);
 })
 
 btn_join.addEventListener('click', ()=>{ 
     const room = ip_room.value
+    my_name = ip_name.value
     socket.emit("join", room)
+    alert(`Join room ${room} thành công!`)
 })
 
-btn_send.addEventListener("click", ()=>{
+const sendMessage = ()=>{
     const message = ip_message.value
-    const name = ip_name.value
-    socket.emit("message", name + ": " + message)
+    if (!message){
+        return;
+    }
+    const obj = {
+        name: my_name,
+        message: message
+    }
+    socket.emit("message", JSON.stringify(obj));
+    ip_message.value = ''
+    ip_message.focus();
+}
+
+btn_send.addEventListener("click", sendMessage)
+
+ip_message.addEventListener('keydown', (event)=>{
+    if (event.key === "Enter"){
+        sendMessage()
+    }
 })
 
 socket.on("thread", function(data){
+    const obj = JSON.parse(data)
+
     const li = document.createElement("li")
-    li.innerHTML = data;
+    li.innerHTML = obj.message;
+
+    if (obj.name === my_name){
+        li.classList.add("right")
+    }
+
     ul_message.appendChild(li)
 })
